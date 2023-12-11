9883d68005f49c4758900555d65018e0cd4804c2
MelvilPE <melvil.bouron@stagiairesmns.fr>
2023-12-11 11:30:17 +0100

    Ajout de la classe User

---
+++ Debut-WPF/Classes/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debut_WPF
+{
+    public class User
+    {
+        string Name { get; set; }
+        int Score { get; set; }
+        public User(string nameInit, int scoreInit)
+        {
+            Name = nameInit;
+            Score = scoreInit;
+        }
+    }
+}
